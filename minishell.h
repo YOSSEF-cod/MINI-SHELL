cad6f51f853d3aa3fedb8c844ffb03901ef54c1a
Youssef Bounite <ybounite@c3r4p4.1337.ma>
2025-02-23 14:52:54 +0000

    adding in head

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/23 13:49:16 by ybounite          #+#    #+#             */
+/*   Updated: 2025/02/23 13:57:33 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./libft/libft.h"
+# include "get_next_line.h"
+# include <stdio.h>
+# include <stdlib.h>
+
+
+# endif
